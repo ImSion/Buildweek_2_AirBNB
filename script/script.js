d95f18643f4fda10d6d759705fa1ab6408685c7b
ImSion <Sion931@gmail.com>
2024-04-10 22:09:06 +0200

    aggiunto il santo bottone search

--- script/script.js
+++ script/script.js
@@ -1,8 +1,19 @@
-document.getElementById('searchButton').addEventListener('click', function() {
-    var searchSection = document.getElementById('searchSection');
-    if (searchSection.style.maxHeight === "0px" || searchSection.style.maxHeight === "") {
-        searchSection.style.maxHeight = searchSection.scrollHeight + "px";
-    } else {
-        searchSection.style.maxHeight = "0px";
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('toggleSearch').addEventListener('click', function() {
+        var searchSection = document.getElementById('searchFields');
+        
+        if (searchSection.classList.contains('hidden')) {
+            searchSection.classList.remove('hidden');
+            setTimeout(function() { // Aspetta un ciclo di rendering per l'animazione
+                searchSection.style.maxHeight = searchSection.scrollHeight + "px";
+            }, 10);
+        } else {
+            searchSection.style.maxHeight = '0';
+            searchSection.addEventListener('transitionend', function() {
+                searchSection.classList.add('hidden');
+            }, {
+                once: true
+            });
+        }
+    });
 });
